577cb90f69ceb208a7928fe1e3770a78de222305
Cara O'Shea <oshea.cara@gmail.com>
2018-02-28 02:46:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value)
+{
+  return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key)
+{
+  var newObject = Object.assign({}, object);
+  delete newObject.key;
+  return newObject[key];
+}
+
+function destructivelyDeleteFromObjectByKey(object, key)
+{
+  object[key] = value;
+  delete object.key;
+  return object;
+}
+
+
+
+
+
+
+
+
+
+
+
